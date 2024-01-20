f0e2cdb7be3768ef40299353fcd0d4f10c4f9bf5
João Manoel <joaomanoeldev@gmail.com>
2024-01-20 19:06:13 -0300

    feat: creating header component

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from "@/components/navbar"
+
+export const Header = () => {
+  return (
+    <header
+      className="
+        container max-w-7xl w-full flex justify-between items-center h-20 fixed z-10 right-0 bottom-0 left-0 border-t bg-background lg:top-0 lg:border-0
+      "
+    >
+      <a href="#" className="text-2xl font-bold">
+        {"<João />"}
+      </a>
+
+      <Navbar />
+    </header>
+  )
+}
