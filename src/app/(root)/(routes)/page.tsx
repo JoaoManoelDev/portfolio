2e53e3eb5bfa0ea79f84d5c2edb2ccd2e040e8c3
João Manoel <joaomanoeldev@gmail.com>
2024-03-21 18:40:32 -0300

    feat: add Projects

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,11 +1,12 @@
-import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
+import { Projects } from "@/components/projects"
 
 const Home = () => {
   return (
     <>
-      <main className="container max-w-7xl lg:pt-20">
+      <main className="container max-w-7xl px-4 lg:px-8 lg:pt-20 ">
         <Hero />
+        <Projects />
       </main>
     </>
   )
