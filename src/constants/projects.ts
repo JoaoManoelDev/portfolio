2bc39e061449debb5413f625514958d07686277b
João Manoel <joaomanoeldev@gmail.com>
2024-03-21 18:40:45 -0300

    feat: create projects data

---
+++ src/constants/projects.ts
@@ -0,0 +1,58 @@
+export const projects = [
+  {
+    id: "1",
+    title: "Timer",
+    description: "App estilo pomodoro feito em React.js",
+    technologies: ["React.js", "React Hook Form", "React Router Dom", "Zod"],
+    images: [
+      {
+        "name": "home",
+        "url": "https://utfs.io/f/4b7953c0-7756-44d4-a35e-f9db54d941c5-k1ehgg.png"
+      },
+      {
+        "name": "home2",
+        "url": "https://utfs.io/f/20434ee4-271f-478b-94a5-d9eb196ca252-k1ehgf.png"
+      },
+      {
+        "name": "history",
+        "url": "https://utfs.io/f/20b194d8-05ff-4b4e-a46d-d78b1704e039-fbvez8.png"
+      }
+    ]
+  },
+  {
+    id: "2",
+    title: "Coffee Delivery",
+    description: "Sistema de Delivery de café realizado na rocketseat.",
+    technologies: ["React.js", "React Hook Form", "React Router Dom", "Zod", "Tailwind CSS"],
+    images: [
+      {
+        "name": "home",
+        "url": "https://utfs.io/f/aa53ca8f-ca37-42a4-8dca-e7431112de8b-1wrmn.png"
+      },
+    ]
+  },
+  {
+    id: "2",
+    title: "Coffee Delivery",
+    description: "Sistema de Delivery de café realizado na rocketseat.",
+    technologies: ["React.js", "React Hook Form", "React Router Dom", "Zod", "Tailwind CSS"],
+    images: [
+      {
+        "name": "home",
+        "url": "https://utfs.io/f/aa53ca8f-ca37-42a4-8dca-e7431112de8b-1wrmn.png"
+      },
+    ]
+  },
+  {
+    id: "2",
+    title: "Coffee Delivery",
+    description: "Sistema de Delivery de café realizado na rocketseat.",
+    technologies: ["React.js", "React Hook Form", "React Router Dom", "Zod", "Tailwind CSS"],
+    images: [
+      {
+        "name": "home",
+        "url": "https://utfs.io/f/aa53ca8f-ca37-42a4-8dca-e7431112de8b-1wrmn.png"
+      },
+    ]
+  }
+]
